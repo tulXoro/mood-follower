eac4fb52693748810be9cca17ee90ba8ee742a0b
Tim <tulxoro@hotmail.com>
2025-01-27 12:04:21 -0800

    added temporary delete account button

--- app/screens/Settings.tsx
+++ app/screens/Settings.tsx
@@ -1,8 +1,9 @@
 import { View, Text, Button } from "react-native";
 import React, { useEffect } from "react";
 import { NavigationProp } from "@react-navigation/native";
-import { FIREBASE_AUTH } from "../../firebaseConfig";
+import { FIREBASE_AUTH, FIREBASE_DB } from "../../firebaseConfig";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { deleteDoc, doc, getDoc, updateDoc } from "firebase/firestore";
 
 interface SettingsProps {
   navigation: NavigationProp<any, any>;
@@ -20,6 +21,46 @@ const Settings = ({ navigation }: SettingsProps) => {
       console.error("Error signing out: ", e);
     }
   }
+
+  const deleteAccount = async () => {
+    try {
+      await FIREBASE_AUTH.currentUser?.delete();
+      // get the user's friends
+      const userId = FIREBASE_AUTH.currentUser?.uid;
+      if (userId) {
+        const friends = await getDoc(doc(FIREBASE_DB, "users", userId));
+        // go through each friend and delete the user from their friends list
+        friends.data()?.friends.forEach(async (friend: string) => {
+          const friendDoc = await getDoc(doc(FIREBASE_DB, "users", friend));
+          const newFriends = friendDoc.data()?.friends.filter((f: string) => f !== userId);
+          await updateDoc(doc(FIREBASE_DB, "users", friend), { friends: newFriends });
+        });
+        // delete the user from the database
+        await deleteDoc(doc(FIREBASE_DB, "users", userId));
+        await AsyncStorage.clear();
+
+        try {
+        // delete the user's authentification
+        await FIREBASE_AUTH.currentUser?.delete();
+        } catch (e) {
+          console.log("Error deleting user: ", e);
+          
+        }
+
+        // remove friend requests containing the user
+
+        alert("Account deleted successfully");
+        navigation.navigate("Login");
+
+      } else {
+        console.error("User ID is undefined");
+      }
+      
+    } catch (e) {
+      console.error("Error deleting account: ", e);
+    }
+  }
+
 
   useEffect(() => {
     AsyncStorage.getItem("displayName").then((value) => setDisplayName(value));
@@ -39,6 +80,7 @@ const Settings = ({ navigation }: SettingsProps) => {
       <View className="p-20 gap-6">
       <Button onPress={() => navigation.navigate("Home")} title="Go Home" />
       <Button onPress={signOut} title="Logout" /></View>
+      <Button onPress={deleteAccount} title="Delete Account" />
 
     </View>
   );
